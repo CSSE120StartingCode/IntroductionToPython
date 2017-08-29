219dc1571aa794fb7ce00c73c4f7e8f83981392e
David Fisher <fisherds@rose-hulman.edu>
2017-08-29 14:28:13 -0400

    m1 comments

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -1,12 +1,11 @@
 ########################################################################
 #
-# TODO: 1.
-#   (Yes, that means for YOU to DO things per these instructions:)
-#
 #   Run this module by right clicking anywhere in this window, then select
 #        Run 'name of file'
 #   After running, find the Console tab (below) and confirm that everything
-#   did indeed get printed (displayed).
+#   did indeed get printed (displayed).  This module is just an example.
+#   Once you have run the file, feel free to play with it some, then you
+#   can move on to m2.
 #
 ########################################################################
 
